b01fb2fa99c32e2351c96fdac31fd1562fde9d4b
Hernan Quintana <hernan@jsatch.com>
2016-11-23 12:13:27 -0500

    fixing url mlab without bd

--- src/main/java/pe/edu/ulima/ejemploapp/Parametros.java
+++ src/main/java/pe/edu/ulima/ejemploapp/Parametros.java
@@ -6,7 +6,7 @@ public class Parametros {
     public static final String PWD_MONGODB = "evaluaciones";
     public static final String PORT_MONGODB = "63387";
     
-    public static final String URL_MONGODB = "mongodb://evaluaciones:evaluaciones@ds163387.mlab.com:63387/evaluaciones";
+    public static final String URL_MONGODB = "mongodb://evaluaciones:evaluaciones@ds163387.mlab.com:63387";
 //    public static final String URL_MONGODB = 
 //            String.format("mongodb://%s:%s@ds163387.mlab.com:%s/evaluaciones", 
 //                USER_MONGODB, PWD_MONGODB, PORT_MONGODB);
