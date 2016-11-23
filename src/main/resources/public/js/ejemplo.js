1535c803a151c6729eee6253820543fcd3eea50e
Hernan Quintana <hernan@jsatch.com>
2016-11-23 12:31:11 -0500

    appending data from mlab

--- src/main/resources/public/js/ejemplo.js
+++ src/main/resources/public/js/ejemplo.js
@@ -1,5 +1,36 @@
 var obtenerEvaluaciones = function () {
     $.get("/evaluaciones", function (data) {
+        var evaluaciones = JSON.parse(data).evaluaciones;
+        
+        evaluaciones.forEach(function(eva){
+            var templateEva = '<div class="panel panel-default">'+
+            '<div class="panel-body">'+
+            '<div class="row">' +
+              '<div class="col-md-3">'+
+                '<h4>' + eva.nommbre + '</h4>'+
+                '<h5>' + eva.ciclo +  ' </h5>'+
+              '</div>'+
+              '<div class="col-md-9">'+
+                '<div class="btn-group pull-right" role="group" aria-label="...">'+
+                  '<button type="button" class="btn btn-default">'+
+                    '<span class="glyphicon glyphicon-pencil" >'+
+                    '</span>'+
+                  '</button>'+
+                  '<button type="button" class="btn btn-default">'+
+                    '<span class="glyphicon glyphicon-ok" >'+
+                    '</span>'+
+                  '</button>'+
+                  '<button type="button" class="btn btn-default">'+
+                    '<span class="glyphicon glyphicon-th-list" >'+
+                    '</span>'+
+                  '</button>'+
+                '</div>'+
+              '</div>'+
+            '</div>'+
+          '</div>'+
+        '</div>';
+            $('#main').append(templateEva);
+        });
         console.log(data);
     });
 };
